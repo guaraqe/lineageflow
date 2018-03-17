fa25844f47fe60b76999eafc21f47df9a9685d40
guaraqe <mail@guaraqe.gq>
2018-03-17 11:14:39 +0100

    statistics: std and without with

--- algorithms/lineageflow-statistics/src/LineageFlow/Statistics/Sample.hs
+++ algorithms/lineageflow-statistics/src/LineageFlow/Statistics/Sample.hs
@@ -1,15 +1,22 @@
 module LineageFlow.Statistics.Sample
   ( nanApplyWith
   , projectWith
+  , mean
   , meanWith
+  , meanDSum
   , meanDSumWith
+  , std
+  , stdWith
+  , stdDSum
+  , meanStd
   , meanStdWith
+  , meanStdDSum
   , meanStdDSumWith
   ) where
 
 import LineageFlow.Prelude
 import qualified LineageFlow.ArrayU as ArrayU
-import Statistics.Sample (meanVarianceUnb, mean)
+import qualified Statistics.Sample as Sample
 import Control.Lens (_2)
 
 --------------------------------------------------------------------------------
@@ -31,11 +38,22 @@ projectWith f = _fmap f . getCompose
 
 --------------------------------------------------------------------------------
 
+mean ::
+  Array i Scalar -> Scalar
+mean = meanWith id
+{-# INLINE mean #-}
+
 meanWith ::
   Storable a =>
   (a -> Scalar) -> Array i a -> Scalar
-meanWith f = nanApplyWith f mean
+meanWith f = nanApplyWith f Sample.mean
 {-# INLINE meanWith #-}
+
+meanDSum ::
+  (CFoldable f, Dom f (Array j Scalar)) =>
+  (f .:. Array j) Scalar -> Scalar
+meanDSum = meanDSumWith id
+{-# INLINE meanDSum #-}
 
 meanDSumWith ::
   (Storable a, CFoldable f, Dom f (Array j a)) =>
@@ -44,11 +62,51 @@ meanDSumWith f =
   meanWith f . mkIx . ArrayU.concat . fmap unIx . _toList . getCompose
 {-# INLINE meanDSumWith #-}
 
+--------------------------------------------------------------------------------
+
+std ::
+  Array i Scalar -> Scalar
+std = stdWith id
+{-# INLINE std #-}
+
+stdWith ::
+  Storable a =>
+  (a -> Scalar) -> Array i a -> Scalar
+stdWith f = nanApplyWith f Sample.stdDev
+{-# INLINE stdWith #-}
+
+stdDSum ::
+  (CFoldable f, Dom f (Array j Scalar)) =>
+  (f .:. Array j) Scalar -> Scalar
+stdDSum = stdDSumWith id
+{-# INLINE stdDSum #-}
+
+stdDSumWith ::
+  Storable a =>
+  (Storable a, CFoldable f, Dom f (Array j a)) =>
+  (a -> Scalar) -> (f .:. Array j) a -> Scalar
+stdDSumWith f =
+  stdWith f . mkIx . ArrayU.concat . fmap unIx . _toList . getCompose
+{-# INLINE stdDSumWith #-}
+
+--------------------------------------------------------------------------------
+
+meanStd ::
+  Array i Scalar -> (Scalar, Scalar)
+meanStd = meanStdWith id
+{-# INLINE meanStd #-}
+
 meanStdWith ::
   Storable a =>
   (a -> Scalar) -> Array i a -> (Scalar, Scalar)
-meanStdWith f = over _2 sqrt . nanApplyWith f meanVarianceUnb
+meanStdWith f = over _2 sqrt . nanApplyWith f Sample.meanVarianceUnb
 {-# INLINE meanStdWith #-}
+
+meanStdDSum ::
+  (CFoldable f, Dom f (Array j Scalar)) =>
+  (f .:. Array j) Scalar -> (Scalar, Scalar)
+meanStdDSum = meanStdDSumWith id
+{-# INLINE meanStdDSum #-}
 
 meanStdDSumWith ::
   (Storable a, CFoldable f, Dom f (Array j a)) =>
